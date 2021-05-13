7ea0c86f5de040c09adc7dcfa8781ef56c87289f
Edoardo Carollo <edoardocarollo1999@gmail.com>
2021-05-13 10:59:49 +0200

    Initial Commit:

---
+++ src/test/java/it/noteatyet/gestionale/GestionaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.noteatyet.gestionale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
